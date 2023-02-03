7ecaf008129238a5051cc5148133f03107ea084a
HyunJoon_Cho <9hjcho2@gmail.com>
2023-02-03 13:52:43 +0900

    업로드 경로 ec2서버로 변경(/usr/upload)

--- second/src/main/java/com/example/demo/MyWebConfig.java
+++ second/src/main/java/com/example/demo/MyWebConfig.java
@@ -6,14 +6,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MyWebConfig implements WebMvcConfigurer {
+	
+	//for window
+	//static final public String savePath = "c:/upload/";
+	
+	//for linux
+	static final public String savePath = "/usr/upload";
+	
 
 	
 //	<resources mapping="/upload/**" location=">
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/upload/**").addResourceLocations("file:///c:/upload/");
-//		WebMvcConfigurer.super.addResourceHandlers(registry);
+		
+		//for window
+		//registry.addResourceHandler("/upload/**").addResourceLocations("file:///c:/upload/");
+		//WebMvcConfigurer.super.addResourceHandlers(registry);
+		
+		//for linux
+		registry.addResourceHandler("/upload/**").addResourceLocations("file:/usr/upload/");
+		
 	}
-
 }
